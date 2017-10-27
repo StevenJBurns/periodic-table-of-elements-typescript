d50eae4ef598df607ce4df1783d5452698e224ca
stevenjburns@msn.com <stevenjburns@msn.com>
2017-10-27 14:22:01 -0600

    Pushing original WPF code to Cocoa repo; major UI overhaul required for XCODE

---
+++ Application.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using Science.Chemistry;
+
+namespace Science
+	{
+	class App : Application
+		{
+		public App()
+			{
+			}
+		
+		[STAThread]
+		static void Main()
+			{
+			App app = new App();
+			app.Run(new WindowPeriodicTable());
+			}
+		
+		}
+		
+	}
